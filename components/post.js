7c22da4452460db9c59b5cce3dadc22ae052c253
MR <a.m.rasouli.n@gmail.com>
2023-09-24 03:26:26 +0330

    Added next and previous link btns to post component

--- components/post.js
+++ components/post.js
@@ -1,11 +1,38 @@
 
 import Title from './title'
 
+import styles from '../styles/Main.module.css';
+import Link from 'next/link';
+
 export default function Post(props) {
+
+    let title_footer = false;
+    if (props.next_link || props.previous_link) {
+        title_footer = true;
+    }
 
     return (
         <>
-            <Title h1={props.main_title} h2={props.sub_title} hr={true} ljust={false} />
+            <Title h1={props.main_title} h2={props.sub_title} ljust={false} >
+                <div className={styles.title_footer}>
+                    {title_footer ? 
+                        <>
+                        <hr />
+                        {props.previous_link ?
+                            <Link href={props.previous_link}><i className="large angle double left icon"></i></Link>
+                        :
+                        null}
+                        <span>&nbsp;</span>
+                        {props.next_link ?
+                            <Link href={props.next_link}><i className={"large angle double right icon " + styles.float_right}></i></Link>
+                        :
+                        null}
+                        <hr />
+                        </>
+                    : 
+                    <hr />}
+                </div>
+            </Title>
             {props.children}
         </>
     )
--- components/title.js
+++ components/title.js
@@ -8,6 +8,9 @@ export default function Title(props) {
             {props.h2 ? <h2 className={props.ljust ? styles.ljust : null}>{props.h2}</h2> : null}
             {props.h3 ? <h3 className={props.ljust ? styles.ljust : null}>{props.h3}</h3> : null}
             {props.hr == true ? <hr /> : null}
+            <>
+                {props.children}
+            </>
         </>
     )
 }
